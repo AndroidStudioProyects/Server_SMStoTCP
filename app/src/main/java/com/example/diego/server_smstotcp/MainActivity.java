bda1d14379eacda2c9cde646576a095d8fccbef7
Diego Giovanazzi <diegogiovanazzi@gmail.com>
2015-05-06 20:15:43 -0300

    funcional

--- app/src/main/java/com/example/diego/server_smstotcp/MainActivity.java
+++ app/src/main/java/com/example/diego/server_smstotcp/MainActivity.java
@@ -4,17 +4,28 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.support.v7.app.ActionBarActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.telephony.SmsMessage;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -22,8 +33,11 @@ public class MainActivity extends ActionBarActivity {
     Switch switch_Server;
     TextView textMensajes;
     EditText edit_IP,edit_Puert;
-
-
+    Button button;
+    ClienteTask hilo;
+    Socket socket;
+    String IP;
+    int Port;
     BroadcastReceiver Broad;
 
     @Override
@@ -38,38 +52,125 @@ public class MainActivity extends ActionBarActivity {
         
     }
 
+    private void ClienteTCP() {
+
+
+
+
+    }
+
 
     private void XML() {
         switch_Server= (Switch) findViewById(R.id.switch_Server);
         textMensajes= (TextView) findViewById(R.id.textMensajes);
         edit_IP= (EditText) findViewById(R.id.edit_IP);
+        IP=edit_IP.getText().toString();
         edit_Puert= (EditText) findViewById(R.id.edit_Puerto);
+        Port=Integer.parseInt(edit_Puert.getText().toString());
+        button= (Button) findViewById(R.id.button);
+        Toast.makeText(getApplicationContext(),"IP:"+IP+" Puerto:"+Port,Toast.LENGTH_SHORT).show();
+
+
 
     }
 
 
     private void BOTONES() {
 
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                hilo=new ClienteTask();
+                hilo.execute();
+                button.setEnabled(true);
+            }
+        });
+
         switch_Server.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if(isChecked){
+                if (isChecked) {
                     Toast.makeText(getApplicationContext(), "Servidor OnLine", Toast.LENGTH_SHORT).show();
                     switch_Server.setText("Server On");
                     registerReceiver(Broad, new IntentFilter("android.provider.Telephony.SMS_RECEIVED"));//POWER_CONNECTED
                     textMensajes.setEnabled(true);
                     textMensajes.append("Servidor Activado \n");
-           }else{
+
+
+                } else {
                     Toast.makeText(getApplicationContext(), "Servidor OffLine", Toast.LENGTH_SHORT).show();
                     switch_Server.setText("Server Off");
                     unregisterReceiver(Broad);// de apaga el broadcastReceiver
                     textMensajes.setEnabled(false);
                     textMensajes.append("Servidor Desactivado \n");
+
+                    hilo.cancel(true);
+                 //   button.setEnabled(true);
+
+                }
             }
-        }
         });
     }
 
+    public class ClienteTask extends AsyncTask<String,Void,String>{
+
+        Boolean control=true;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            control=true;
+            Toast.makeText(getApplicationContext(), "onPreExecute", Toast.LENGTH_SHORT).show();
+
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            String result = null;
+            try {
+                //Create a client socket and define internet address and the port of the server
+                socket = new Socket("192.168.0.103",9001);
+                //Get the input stream of the client socket
+                InputStream is = socket.getInputStream();
+                //Get the output stream of the client socket
+                PrintWriter out = new PrintWriter(socket.getOutputStream(),true);
+                //Write data to the output stream of the client socket
+                out.println(params[0]);
+                //Buffer the data coming from the input stream
+                BufferedReader br = new BufferedReader(
+                        new InputStreamReader(is));
+                //Read data in the input buffer
+                result = br.readLine();
+                //Close the client socket
+                socket.close();
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+            } catch (UnknownHostException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return result;
+        }
+
+
+        protected void onProgressUpdate(String values) {
+            Toast.makeText(getApplicationContext(), "onProgressUpdate", Toast.LENGTH_SHORT).show();
+
+        }
+
+        @Override
+        protected void onPostExecute(String o) {
+            Toast.makeText(getApplicationContext(), "onPostExecute", Toast.LENGTH_SHORT).show();
+            textMensajes.append("\n" + o);
+        }
+
+        @Override
+        protected void onCancelled(String o) {
+            Toast.makeText(getApplicationContext(), "onCancelled", Toast.LENGTH_SHORT).show();
+            control=false;
+        }
+    }
 
     private void BROADCAST() {
 
@@ -93,6 +194,8 @@ public class MainActivity extends ActionBarActivity {
                             String senderNum = phoneNumber;
                             String message = currentMessage.getDisplayMessageBody();
                             Toast.makeText(context, "Radiobase: " + senderNum + " Mensaje: " + message, Toast.LENGTH_SHORT).show();
+                            hilo=new ClienteTask();
+                            hilo.execute("Radiobase: " + senderNum + " Alarma: " + message);
                             textMensajes.append("Radiobase:" + senderNum + " Mensaje: " + message+"\n");
                         }
                     }
@@ -151,4 +254,6 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
 }
